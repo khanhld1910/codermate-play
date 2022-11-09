842e82df98f208c0cb1b2b889066e097070fcea6
Khanh Luong <khanhld1910@gmail.com>
2022-11-09 22:56:54 +0700

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,9 +20,43 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: RandomWords(),
         ),
       ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _biggerFont = const TextStyle(fontSize: 18);
+  final _suggestions = <WordPair>[];
+
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
+    // return Text(wordPair.asPascalCase);
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll((generateWordPairs().take(10)));
+        }
+
+        return ListTile(
+            title: Text(
+          _suggestions[index].asPascalCase,
+          style: _biggerFont,
+        ));
+      },
     );
   }
 }
