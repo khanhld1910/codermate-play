80cd18d59bffa956b0221ea7270ee08fbb218fff
Khanh Luong <khanhld1910@gmail.com>
2022-11-12 14:54:40 +0700

    Install Mapbox for flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_map/flutter_map.dart'; // Suitable for most situations
+// import 'package:flutter_map/plugin_api.dart'; // Only import if required functionality is not exposed by default
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'My First Map',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Startup Name Generator'),
+          title: const Text('My First Map'),
         ),
         body: const Center(
           child: RandomWords(),
@@ -35,28 +34,25 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final _biggerFont = const TextStyle(fontSize: 18);
-  final _suggestions = <WordPair>[];
-
   @override
   Widget build(BuildContext context) {
-    // final wordPair = WordPair.random();
-    // return Text(wordPair.asPascalCase);
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return const Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll((generateWordPairs().take(10)));
-        }
-
-        return ListTile(
-            title: Text(
-          _suggestions[index].asPascalCase,
-          style: _biggerFont,
-        ));
-      },
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.509364, -0.128928),
+        zoom: 9.2,
+      ),
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: 'OpenStreetMap contributors',
+          onSourceTapped: null,
+        ),
+      ],
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+      ],
     );
   }
 }
